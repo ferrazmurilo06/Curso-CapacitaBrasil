4f1aab88951b375ee4bc986f4d359c4edfe9d5af
Murilo A Ferraz <muriloalbuquerqueferraz@gmail.com>
2025-03-26 10:04:19 -0300

    atividade de variável booleana.

---
+++ Lógica de Programação/Boolean.py
@@ -0,0 +1,8 @@
+entrada = input("Você é maior de idade? ")
+
+if entrada == 'Sim':
+    print("Tenho 18.")
+elif entrada == 'Não':
+    print("Tenho 15.")
+else:
+    print("Resposta inválida tente novamente.")
