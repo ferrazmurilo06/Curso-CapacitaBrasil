6dd880a29e208d9b47160cf44d24c8ec6d64201b
Murilo A Ferraz <muriloalbuquerqueferraz@gmail.com>
2025-04-01 09:15:49 -0300

    atividade de operadores lógicos.

---
+++ Lógica de Programação/operadores_logicos.py
@@ -0,0 +1,9 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+if a==b:
+    print('Números iguais.')
+elif a != b:
+    print('Números diferentes.')
+else:
+    print('Digite dois números.')
