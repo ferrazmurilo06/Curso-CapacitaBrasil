b14893ed8c973f82c03b8b8a0000167ee73538b4
murilo <muriloalbuquerqueferraz@gmail.com>
2025-06-12 16:12:50 -0300

    adicionando conhecimento

--- Desenvolvimento FullStack-intermediario/script.js
+++ Desenvolvimento FullStack-intermediario/script.js
@@ -1,3 +1,22 @@
-console.log("Hello World!!!!")
+console.log("Hello World!!!!");
 
-alert("Bem vindo ao JavaScript!!!!")
+alert("Bem vindo ao JavaScript!!!!");
+
+// comentário
+/*
+comentário de bloco.
+*/ 
+
+//aula01
+
+var idade = 21;
+let serie = "quarto semestre";
+const nome = "Murilo";
+
+//aula02
+
+let nome02 = "joão";
+let idadej = 22;
+let solteiro = true;
+let emprego = null;
+let sonho;
